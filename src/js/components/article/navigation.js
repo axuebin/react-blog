bf8736bffde718110f38f2a5301fc1e67d117a84
axuebin <xbmail2013@163.com>
2017-09-19 13:40:16 +0800

    :lipstick: Modify img and table style

--- src/js/components/article/navigation.js
+++ src/js/components/article/navigation.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import marked from 'marked';
 
-function getTitleText(inputStr){
-  return inputStr.split('>')[1].split('<')[0];
-}
+// function getTitleText(inputStr){
+//   return inputStr.split('>')[1].split('<')[0];
+// }
 
 export default class Navigation extends React.Component {
-  componentDidMount(){
-    const content = marked(this.props.content);
-    const h2 = content.match(new RegExp('<h2 id="-">([^<]*)</h2>','g')).map(item => getTitleText(item));
-    const h3 = content.match(new RegExp('<h3 id="-">([^<]*)</h3>','g')).map(item => getTitleText(item));
-    const h4 = content.match(new RegExp('<h4 id="-">([^<]*)</h4>','g')).map(item => getTitleText(item));
-  }
+  // componentDidMount(){
+  //   const content = marked(this.props.content);
+  //   const h2 = content.match(new RegExp('<h2 id="-">([^<]*)</h2>','g')).map(item => getTitleText(item));
+  //   const h3 = content.match(new RegExp('<h3 id="-">([^<]*)</h3>','g')).map(item => getTitleText(item));
+  //   const h4 = content.match(new RegExp('<h4 id="-">([^<]*)</h4>','g')).map(item => getTitleText(item));
+  // }
 
   render() {
     return (
