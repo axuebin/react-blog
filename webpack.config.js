13d4b64557e3bf6a67aa7c3cf59d46d0a37a5491
axuebin <xbmail2013@163.com>
2017-09-21 15:57:48 +0800

    :wrench: Change output path

--- webpack.config.js
+++ webpack.config.js
@@ -14,21 +14,25 @@ module.exports = {
         query: {
           presets: ['react', 'es2015']
         }
-      },{
+      }, {
         test: /\.css$/,
         loader: 'style-loader!css-loader'
-      },{
+      }, {
         test: /\.js$/,
         exclude: /(node_modules)/,
         loader: 'eslint-loader'
-      },{
+      }, {
         test: /\.json$/,
         loader: 'json-loader'
       }
     ]
   },
   output: {
-    path: __dirname + "/src/",
-    filename: "bundle.js"
+    path: path.resolve(__dirname, './dist'),
+    filename: 'js/[name].js',
+    publicPath: './dist/',
+    chunkFilename: 'js/async/[name].js'
+    // path: __dirname + "/src/",
+    // filename: "bundle.js"
   }
 }
