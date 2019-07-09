1504e5a8bd37bdfb6b846bbc252115adaac5efe7
axuebin <xbmail2013@163.com>
2019-07-09 19:04:18 +0800

    feat(博客页面): tip

---
+++ src/components/blogPage/Tip/index.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react'
+import styles from './index.module.scss'
+
+function Tip() {
+  return (
+    <div className={styles.tip}>
+      <div className={styles.container}>
+        <div className={styles.title}>
+          听...
+        </div>
+        <div className={styles.content}>
+          海哭的声音...
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Tip;
