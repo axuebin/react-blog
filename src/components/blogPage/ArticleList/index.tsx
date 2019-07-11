bd3e705630d36818fcd10669acfc5c3f69d5feba
axuebin <xbmail2013@163.com>
2019-07-11 13:07:07 +0800

    feat(博客页面): 分页

--- src/components/blogPage/ArticleList/index.tsx
+++ src/components/blogPage/ArticleList/index.tsx
@@ -9,6 +9,8 @@ interface IState {
   articleList: ArticleInterface[]
   page: number
   pageSize: number
+  count: number
+  moreLock: boolean
 }
 interface IProps extends RouteComponentProps {
 }
@@ -20,6 +22,8 @@ class ArticleList extends React.Component<IProps, IState> {
       articleList: [],
       page: 0,
       pageSize: 5,
+      count: 0,
+      moreLock: false
     }
   }
   componentDidMount() {
@@ -29,16 +33,41 @@ class ArticleList extends React.Component<IProps, IState> {
       pageSize
     }).then(res => {
       if (res.success && res.data && Object.keys(res.data).length > 0) {
-        const { data } = res.data;
+        const { count, data } = res.data;
         this.setState({
+          count,
           articleList: data,
         })
       }
     })
   }
+  more() {
+    const { page, pageSize, articleList, moreLock } = this.state
+    const nextPage = page + 1
+    if (!moreLock) {
+      this.setState({ moreLock: true })
+      getArticleList({
+        page: nextPage,
+        pageSize
+      }).then(res => {
+        if (res.success && res.data && Object.keys(res.data).length > 0) {
+          const { count, data } = res.data;
+          const temp = articleList.concat(data)
+          this.setState({
+            count,
+            articleList: temp,
+            page: nextPage,
+            moreLock: false
+          })
+        }
+      }).catch(() => {
+        this.setState({ moreLock: false })
+      })
+    }
+  }
   render() {
     const { history } = this.props
-    const articleList = this.state.articleList
+    const { articleList, count } = this.state
     return (
       <div className={styles.articleList}>
         {
@@ -49,6 +78,13 @@ class ArticleList extends React.Component<IProps, IState> {
               ></ArticleItem>
             )
           )
+        }
+        {
+          articleList.length < count ? 
+          <div className={styles.more} onClick={() => this.more()}>
+            <div className={styles.text}>查看更多</div>
+          </div>
+          : <div className={styles.nomore}>没有更多了，我会继续加油哒~</div>
         }
       </div>
     );
